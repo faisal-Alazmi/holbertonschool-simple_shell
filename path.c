dfb720291dc05fdb8a14cb87386804b5191b90ec
root <root@ip-10-42-173-160.ec2.internal>
2025-11-11 15:18:53 +0000

    fix betty

--- path.c
+++ path.c
@@ -1,38 +1,38 @@
 #include "shell.h"
 
 /**
- * find_path - Locate command using PATH environment variable
- * @command: the command to locate
- *
- * Return: full executable path if found, or NULL if not
- */
+	* find_path - Locate command using PATH environment variable
+	* @command: the command to locate
+	*
+	* Return: full executable path if found, or NULL if not
+	*/
 char *find_path(char *command)
 {
-    char *path_env, *path_copy, *dir;
-    char full_path[1024];
+	char *path_env, *path_copy, *dir;
+	char full_path[1024];
 
-    /* if command includes '/' check directly */
-    if (access(command, X_OK) == 0)
-        return (strdup(command));
+	/* if command includes '/' check directly */
+	if (access(command, X_OK) == 0)
+	return (strdup(command));
 
-    path_env = getenv("PATH");
-    if (!path_env)
-        return (NULL);
+	path_env = getenv("PATH");
+	if (!path_env)
+	return (NULL);
 
-    path_copy = strdup(path_env);
-    dir = strtok(path_copy, ":");
+	path_copy = strdup(path_env);
+	dir = strtok(path_copy, ":");
 
-    while (dir)
-    {
-        snprintf(full_path, sizeof(full_path), "%s/%s", dir, command);
-        if (access(full_path, X_OK) == 0)
-        {
-            free(path_copy);
-            return (strdup(full_path));
-        }
-        dir = strtok(NULL, ":");
-    }
+	while (dir)
+	{
+	snprintf(full_path, sizeof(full_path), "%s/%s", dir, command);
+	if (access(full_path, X_OK) == 0)
+	{
+	free(path_copy);
+	return (strdup(full_path));
+	}
+	dir = strtok(NULL, ":");
+	}
 
-    free(path_copy);
-    return (NULL);
+	free(path_copy);
+	return (NULL);
 }
