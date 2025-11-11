5912f3284e4e413b2ae4af9b5a2f66bb46a841dc
root <root@ip-10-42-224-11.ec2.internal>
2025-11-11 14:34:59 +0000

    fix task3

--- parse_line.c
+++ parse_line.c
@@ -1,42 +1,39 @@
+#include <stdlib.h>
+#include <string.h>
 #include "shell.h"
 
-/**
-	* parse_line - Split line into arguments
-	* @line: Line to parse
-	*
-	* Return: Array of arguments
-	*/
 char **parse_line(char *line)
 {
-	int bufsize = MAX_ARGS, position = 0;
-	char **tokens = malloc(bufsize * sizeof(char *));
-	char *token;
+    int bufsize = MAX_ARGS, position = 0;
+    char **tokens = malloc(bufsize * sizeof(char *));
+    char *token;
 
-	if (!tokens)
-	{
-	perror("allocation error");
-	exit(EXIT_FAILURE);
-	}
+    if (!tokens)
+    {
+        perror("allocation error");
+        exit(EXIT_FAILURE);
+    }
 
-	token = strtok(line, " \t\r\n\a");
-	while (token != NULL)
-	{
-	tokens[position] = token;
-	position++;
+    line[strcspn(line, "\n")] = '\0';
 
-	if (position >= bufsize)
-	{
-	bufsize += MAX_ARGS;
-	tokens = realloc(tokens, bufsize * sizeof(char *));
-	if (!tokens)
-	{
-	perror("allocation error");
-	exit(EXIT_FAILURE);
-	}
-	}
+    token = strtok(line, " \t\r\n\a");
+    while (token != NULL)
+    {
+        tokens[position] = token;
+        position++;
 
-	token = strtok(NULL, " \t\r\n\a");
-	}
-	tokens[position] = NULL;
-	return (tokens);
+        if (position >= bufsize)
+        {
+            bufsize += MAX_ARGS;
+            tokens = realloc(tokens, bufsize * sizeof(char *));
+            if (!tokens)
+            {
+                perror("allocation error");
+                exit(EXIT_FAILURE);
+            }
+        }
+        token = strtok(NULL, " \t\r\n\a");
+    }
+    tokens[position] = NULL;
+    return (tokens);
 }
