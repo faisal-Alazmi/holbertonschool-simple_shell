adb4d57bbb70015c22636d0a240a9cfab5e48612
user76335 <11958@holbertonstudents.com>
2025-11-12 16:29:56 +0000

    fix code

--- finalize_tokens.c
+++ finalize_tokens.c
@@ -1,3 +1,17 @@
+#include <stdlib.h>
+
+/**
+ * finalize_tokens - null-terminates the array and frees the duplicated string
+ * @argv: pointer to the token array
+ * @i: number of tokens stored in argv
+ * @work: duplicated input string
+ *
+ * This function sets the next element after the last token to NULL,
+ * frees the duplicated string used for tokenization, and returns argv.
+ * If no tokens were found, it frees argv and returns NULL.
+ *
+ * Return: pointer to the token array, or NULL if no tokens were found
+ */
 char **finalize_tokens(char **argv, size_t i, char *work)
 {
 	argv[i] = NULL;
