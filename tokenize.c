70463b7fa7af249e49a0bc03d9b884e73ca360d6
user76335 <11958@holbertonstudents.com>
2025-11-12 16:15:06 +0000

    fix code

--- tokenize.c
+++ tokenize.c
@@ -1,42 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * tokenize - splits a line into an array of tokens
+ * @line: the input string to tokenize
+ *
+ * Return: a NULL-terminated array of strings (tokens),
+ *         or NULL on failure or if no tokens are found.
+ */
 char **tokenize(char *line)
 {
-	char *work, *tok;
-	char **argv;
-	size_t cap = 8, i = 0;
+    char *work, *tok;
+    char **argv;
+    size_t cap = 8, i = 0;
 
-	if (!line)
-		return (NULL);
+    if (!line)
+        return (NULL);
 
-	argv = malloc(sizeof(char *) * cap);
-	if (!argv)
-		return (NULL);
+    argv = malloc(sizeof(char *) * cap);
+    if (!argv)
+        return (NULL);
 
-	work = _strdup(line);
-	if (!work)
-	{
-		free(argv);
-		return (NULL);
-	}
+    work = strdup(line);  // use standard strdup
+    if (!work)
+    {
+        free(argv);
+        return (NULL);
+    }
 
-	tok = strtok(work, " \t");
-	while (tok)
-	{
-		if (!add_token(&argv, &cap, &i, tok))
-		{
-			free_argv(argv);
-			free(work);
-			return (NULL);
-		}
-		tok = strtok(NULL, " \t");
-	}
+    tok = strtok(work, " \t");
+    while (tok)
+    {
+        if (!add_token(&argv, &cap, &i, tok))  // make sure add_token handles realloc
+        {
+            free_argv(argv);
+            free(work);
+            return (NULL);
+        }
+        tok = strtok(NULL, " \t");
+    }
 
-	argv[i] = NULL;
-	free(work);
+    argv[i] = NULL;  // NULL-terminate the array
+    free(work);
 
-	if (i == 0)
-	{
-		free(argv);
-		return (NULL);
-	}
-	return (argv);
+    if (i == 0)  // no tokens found
+    {
+        free(argv);
+        return (NULL);
+    }
+
+    return (argv);
 }
