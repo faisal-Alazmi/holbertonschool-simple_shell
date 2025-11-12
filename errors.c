4af8ec6e77e143904598163ad9ad4c700a1d9df5
user76335 <11958@holbertonstudents.com>
2025-11-12 16:33:25 +0000

    fix code

--- errors.c
+++ errors.c
@@ -1,22 +1,21 @@
-#include "shell.h"
-
-/**
- * write_num - prints a positive integer via write()
- * @n: number to print
- */
 static void write_num(int n)
 {
-	char buf[20];
-	int i = 0;
-	unsigned int x = (n < 0) ? -n : n;
+    int x = n;
+    char buf[12];
+    int i = 0;
 
-	if (n < 0)
-		write(STDERR_FILENO, "-", 1);
-	if (x == 0)
-	{
-		write(STDERR_FILENO, "0", 1);
-		return;
-	}
-	while (x > 0 && i < (int)sizeof(buf))
-	{
-		buf[i++] = (char)('0' + (x % 1
+    if (x == 0)
+    {
+        buf[i++] = '0';
+    }
+    else
+    {
+        while (x > 0)
+        {
+            buf[i++] = (char)('0' + (x % 10));
+            x /= 10;
+        }
+    }
+    buf[i] = '\0';
+    /* you can print it or do something else */
+}
