fd79f2cd99f3f67996b2eccbf48340fe1c76025b
user76335 <11958@holbertonstudents.com>
2025-11-12 16:26:13 +0000

    fix function

--- finalize_tokens.c
+++ finalize_tokens.c
@@ -1,13 +1,3 @@
-#include <stdlib.h>
-
-/**
- * finalize_tokens - null-terminates the array and frees work buffer
- * @argv: the token array
- * @i: number of tokens stored
- * @work: duplicated input string
- *
- * Return: argv if tokens exist, NULL if none
- */
 char **finalize_tokens(char **argv, size_t i, char *work)
 {
 	argv[i] = NULL;
